15ad80670f2335851786a2d4b75223daa0ed2dd0
Kevin Griffin <griffin.kev@gmail.com>
2010-08-02 10:48:31 -0400

    changed to ci email

--- cruise_config.rb
+++ cruise_config.rb
@@ -2,6 +2,6 @@ Project.configure do |project|
   project.build_command = './ci_build.sh'
    project.email_notifier.emails = ['kgriffin@chariotsolutions.com']
   # Set email 'from' field.
-   project.email_notifier.from = 'kgriffin@chariotsolutions.com'
+   project.email_notifier.from = 'ci@chariotsolutions.com'
 
 end
